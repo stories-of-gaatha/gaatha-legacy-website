80a32fd7a028517073effc9157e4ecf043178ea9
bimal prasad pandey <pandeybi125@gmail.com>
2021-04-30 16:38:06 +0545

    Add work models and migrations

---
+++ apps/work/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from apps.work.models import Tag, WorkType, Picture, Work
+
+
+class TagAdmin(admin.ModelAdmin):
+    """
+    Tag  admin
+    """
+
+    list_display = ["id", "name"]
+    list_display_links = ["id", "name"]
+    search_fields = ["id", "name"]
+
+
+class WorkTypeAdmin(admin.ModelAdmin):
+    """
+    Work type  admin
+    """
+
+    list_display = ["id", "name"]
+    list_display_links = ["id", "name"]
+    search_fields = ["id", "name"]
+
+
+class PictureAdmin(admin.ModelAdmin):
+    """
+    Picture  admin
+    """
+
+    list_display = ["id", "description"]
+    list_display_links = ["id", "description"]
+    search_fields = ["id", "description"]
+
+
+class WorkAdmin(admin.ModelAdmin):
+    """
+    Work  admin
+    """
+
+    list_display = [
+        "id",
+        "artwork",
+        "cover_image",
+        "cover_description",
+        "is_featured_in_dashboard",
+        "work_type",
+    ]
+    list_display_links = ["id"]
+    search_fields = ["id"]
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(WorkType, WorkTypeAdmin)
+admin.site.register(Picture, PictureAdmin)
+admin.site.register(Work, WorkAdmin)
---
+++ apps/work/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    """
+    Tag model
+    """
+
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "A - Tags"
+
+    def __str__(self):
+        return self.name
+
+
+class WorkType(models.Model):
+    """
+    Work type model
+    """
+
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "B - Work types"
+
+    def __str__(self):
+        return self.name
+
+
+class Picture(models.Model):
+    """
+    Picture model
+    """
+
+    image = models.ImageField(upload_to="pictures", max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Picture"
+        verbose_name_plural = "C - Pictures"
+
+    def __str__(self):
+        return self.image.url
+
+
+class Work(models.Model):
+    """
+    Work model
+    """
+
+    artwork = models.FileField(null=True, blank=True, upload_to="artworks")
+    cover_image = models.ImageField(
+        null=True, blank=True, upload_to="cover_images", max_length=255
+    )
+    cover_description = models.TextField()
+    is_featured_in_dashboard = models.BooleanField(default=False)
+    # Work type foreign key
+    work_type = models.ForeignKey(WorkType, on_delete=models.PROTECT)
+    # Tag and pictures
+    tags = models.ManyToManyField(Tag)
+    picture = models.ManyToManyField(Picture)
+
+    class Meta:
+        verbose_name = "Work"
+        verbose_name_plural = "D - Works"
+
+    def __str__(self):
+        # String representation of press dossier
+        return str(self.id)
