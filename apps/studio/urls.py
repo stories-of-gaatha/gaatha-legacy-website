65ffa8e0f9e8ffd33a202fb1c7ff63357a53a6c5
k9845 <khatririshi2430@gmail.com>
2021-05-10 12:55:28 +0545

    Manage url route for studio

--- apps/studio/urls.py
+++ apps/studio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.studio import views
 
 urlpatterns = [
-    path("", views.about, name="about-us"),
+    path("", views.about, name="about_us"),
     path("people/", views.PeopleList.as_view(), name="people_list"),
 ]
