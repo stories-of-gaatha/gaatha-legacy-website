ecfb47044cd85518ff84a30c16f2e5de4bb2b015
barshathakuri <barshathakuri708@gmail.com>
2021-05-31 22:13:43 +0545

    styling in home-page layout

--- apps/work/urls.py
+++ apps/work/urls.py
@@ -4,4 +4,5 @@ from apps.work import views
 urlpatterns = [
     path("", views.WorkList.as_view(), name="work_list"),
     path("<pk>/", views.WorkDetail.as_view(), name="work_detail"),
+    path("", views.Contact.as_view(), name="contact"),
 ]
