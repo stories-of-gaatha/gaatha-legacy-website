c9f058abcd1bfceddf4cc0c761b5c9de1dc02bc9
Subina <subina@togglecorp.com>
2021-06-01 22:46:33 +0545

    Remove contact from urls

--- apps/work/urls.py
+++ apps/work/urls.py
@@ -4,5 +4,4 @@ from apps.work import views
 urlpatterns = [
     path("", views.WorkList.as_view(), name="work_list"),
     path("<pk>/", views.WorkDetail.as_view(), name="work_detail"),
-    path("", views.Contact.as_view(), name="contact"),
 ]
