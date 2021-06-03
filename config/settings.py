8aa26bc87c1391b68154d8cf1fcd31ca32e43fcf
Subina <subina@togglecorp.com>
2021-06-03 19:49:42 +0545

    Highlight active tags and types in work pages

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,11 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
+
+            'libraries':{
+                'tag_library': 'templatetags.tag_library',
+            }
         },
     }
 ]
