1376a3dcce004e05d1175670c6d89faf3cad504a
bimal prasad pandey <pandeybi125@gmail.com>
2021-06-04 15:41:13 +0545

    Add lookup filter

--- templatetags/tag_library.py
+++ templatetags/tag_library.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter()
 def to_int(value):
     return int(value)
+
+
+@register.filter(name='lookup')
+def lookup(dictionary, key):
+    """
+    Retrives value from dict
+    """
+    return dictionary.get(key, "")
